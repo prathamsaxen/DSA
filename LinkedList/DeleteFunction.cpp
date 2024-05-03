385a9e1ccd3c7539c0d11a95aef783644996cb7a
Pratham Saxena <er.prathamsaxena@gmail.com>
2024-05-03 18:56:22 +0530

    Uplaoded Delete Function!

---
+++ LinkedList/DeleteFunction.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data=data;
+        this->next=NULL;
+    }
+};
+
+void printLinkedList(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+void DeleteAtPosition(Node *&head,int position)
+{
+    int index=1;
+    Node *temp = head;
+    while(index<=position)
+    {
+        index++;
+        temp=temp->next;
+    }
+    Node* Previos=temp;
+    temp=temp->next;
+    Previos->next=temp->next;
+}
+
+int main()
+{
+    Node *head=new Node(1);
+    Node *node2=new Node(2);
+    Node *node3=new Node(3);
+    Node *node4=new Node(4);
+    head->next=node2;
+    node2->next=node3;
+    node3->next=node4;
+    printLinkedList(head);
+    DeleteAtPosition(head,3);
+    cout<<endl;
+    printLinkedList(head);
+
+
+
+    return 0;
+}
