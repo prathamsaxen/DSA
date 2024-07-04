57b976b3987a227c938cb415039d756cc467a239
Pratham Saxena <er.prathamsaxena@gmail.com>
2024-07-04 18:06:20 +0530

    Added Queue Using STL!

--- Queue/QueueUsingSTL.cpp
+++ Queue/QueueUsingSTL.cpp
@@ -8,5 +8,16 @@ int main()
     cout << "Check Queue is Empty or Not => " << data.empty() << endl;
     cout << "Check Size Of Queue => " << data.size() << endl;
 
+    data.push(2);
+    data.push(4);
+    data.push(6);
+    data.push(8);
+    data.push(10);
+
+    cout << "Front Element => " << data.front() << endl;
+    cout << "Check Size Of Queue => " << data.size() << endl;
+    cout << "Check Queue is Empty or Not => " << data.empty() << endl;
+    cout << "Back Element => " << data.back() << endl;
+
     return 0;
 }
