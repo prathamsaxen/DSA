7f09244a1879d990d05e14a3e835bc4bf7418c4a
Pratham Saxena <er.prathamsaxena@gmail.com>
2024-07-04 18:02:34 +0530

    Queue Started Again! Using STL

---
+++ Queue/QueueUsingSTL.cpp
@@ -0,0 +1,12 @@
+#include <queue>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    queue<int> data;
+    cout << "Check Queue is Empty or Not => " << data.empty() << endl;
+    cout << "Check Size Of Queue => " << data.size() << endl;
+
+    return 0;
+}
